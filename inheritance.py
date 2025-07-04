3fa83b8990e9ca6b7a88a0e736eda4ef7a6574d2
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2025-07-05 00:06:44 +0600

    OOP More

--- inheritance.py
+++ inheritance.py
@@ -1,46 +1,29 @@
-# base class, parent class, common attribute + functionality class
-# derived class, child class, uncommon attribute + functionality class 
+# inheritance provides you "is a" relation
 
-class Gadget:
-    def __init__(self, brand, price, color, origin) -> None:
-        self.brand = brand
-        self.price = price
-        self.color = color
-        self.origin = origin
-    
-    def run(self):
-        return f'Running laptop: {self.brand}'
+class Animal:
+    pass
 
+# Dog is a animal
+class Dog(Animal):
+    pass
 
-class Laptop:
-    def __init__(self, memory, ssd) -> None:
-        self.memory = memory
-        self.ssd = ssd
-
-    def coding(self):
-        return f'learning python and practicing'
-    
-class Phone(Gadget):
-    def __init__(self, brand, price, color, origin, dual_sim) -> None:
-        self.dual_sim = dual_sim
-        super().__init__(brand, price, color, origin)
-    
-    def phone_call(self, number, text):
-        return f'Sending SMS to: {number} with: {text}'
-    
-    def __repr__(self) -> str:
-        return f'phone: {self.brand} {self.price} {self.dual_sim}'
-
-class Camera:
-    def __init__(self, pixel) -> None:
-        self.pixel = pixel
-
-    def change_lens(self):
-        pass
+# Tiger is a animal
+class Tiger(Animal):
+    pass
 
 
-# inheritance
-my_phone = Phone('iphone', 120000, 'silver', 'china', True)
-# my_phone.phone_call()
-print(my_phone.brand)
-print(my_phone)
+
+class Furniture:
+    pass
+
+# chair is a furniture
+class Chair(Furniture):
+    pass
+
+# table is a furniture
+class Table(Furniture):
+    pass
+
+# bed is a furniture
+class Bed(Furniture):
+    pass
