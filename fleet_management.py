78432907fefdec7d6fa522b53fa9118c8bf50581
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-04-06 11:00:06 +0600

    Update fleet_management.py

--- fleet_management.py
+++ fleet_management.py
@@ -1,6 +1,6 @@
 # Ena Poribohon 
 class Company:
-    
+        
     def __init__(self, name, address, ) -> None:
         self.name = name
         self.bus = []
@@ -11,22 +11,22 @@ class Company:
         self.supervisors=[]
         self.fare = []
          
-
+    
 class Driver:
     def __init__(self, name, license, age) -> None:
         self.license = license
         self.name = name
         self.age = age
-
+   
 class Counter:
     def __init__(self) -> None:
         pass
     def purchase_a_ticket(self, start, destination):
         pass
-
+   
 class Passenger:
     pass
-
+  
 class Supervisor:
     pass
 
