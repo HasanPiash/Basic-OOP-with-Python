49e6d83a7fe2750e83b653cc689b459c2a466927
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-04-07 13:45:44 +0600

    Update inner_func.py

--- inner_func.py
+++ inner_func.py
@@ -1,15 +1,15 @@
 # function is a first class object
-
+    
 def double_decker():
     print('starting the double decker')
     def inner_fun():
         print('inside the inner')
         return 3000
     return inner_fun
-
-# print(double_decker())
+      
+# print(double_decker())   
 # print(double_decker()())
-
+    
 def do_something(work):
     print('work started')
     # print(work)
