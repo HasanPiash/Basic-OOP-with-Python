e2f2d5aee112d1d43e217fb0b898b699d709eecd
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-04-07 13:45:57 +0600

    Update multi_level_inheritance.py

--- multi_level_inheritance.py
+++ multi_level_inheritance.py
@@ -2,13 +2,13 @@ class Vehicle:
     def __init__(self, name, price) -> None:
         self.name = name
         self.price = price
- 
+     
     def __repr__(self) -> str:
         return f'{self.name} {self.price}'
-    
+        
     def move(self):
         pass
-
+    
 class Bus(Vehicle):
     def __init__(self, name, price, seat) -> None:
         self.seat = seat
