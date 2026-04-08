5c1418fe6ef44c07f7d7f20228b5b20db3b677cb
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-04-08 19:21:23 +0600

    Update override.py

--- override.py
+++ override.py
@@ -4,37 +4,37 @@ class Person:
         self.age = age
         self.height = height
         self.weight = weight
-
+     
     def eat(self):
         print('vat mangso polau korma')
-
+    
     def exercise(self):
         raise NotImplementedError
-
+     
 class Cricketer(Person):
     def __init__(self, name, age, height, weight, team) -> None:
         self.team = team
         super().__init__(name, age, height, weight)
-    
+        
     # override
     def eat(self):
         print('vegetables')
-
+    
     def exercise(self):
         print('gym e poisa diya hudai gham jorai')
-
+    
     # + sign operator overload
     def __add__(self, other):
         return self.age + other.age
-    
+         
     # * sign operator overload
     def __mul__(self, other):
         return self.weight * other.weight
-    
+        
     # len overload
     def __len__(self):
         return self.height
-    
+        
     # > operator overload
     def __gt__(self, other):
         return self.age > other.age
