b023731789c908501949051eab28c134a31a3f07
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-08-29 11:26:44 +0600

    Update computer.py

--- computer.py
+++ computer.py
@@ -1,5 +1,4 @@
 # inheritance vs composition
-   
 class CPU: 
     def __init__(self, cores) -> None:
         self.cores = cores
@@ -15,12 +14,10 @@ class HardDrive:
 # computer has a cpu
 # computer has a ram
 # computer has a hard drive
-
 class Computer:
     def __init__(self, cores, ram_size, hd_capacity) -> None:
         self.cpu = CPU(cores)
         self.ram = RAM(ram_size)
         self.hard_disc = HardDrive(hd_capacity)
-
 
 mac = Computer(8, 16, 512)
