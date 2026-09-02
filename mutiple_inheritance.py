77d0fb399b26a6e94b9b78966d1703af195955b8
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-09-02 23:14:19 +0600

    Update mutiple_inheritance.py

--- mutiple_inheritance.py
+++ mutiple_inheritance.py
@@ -2,7 +2,6 @@ class Family:
     def __init__(self, address) -> None:
         self.address = address
 
-
 class School:
     def __init__(self, id, level) -> None:
         self.id = id   
@@ -10,10 +9,11 @@ class School:
     
 class Sports:
     def __init__(self, game) -> None:
-        self.game = game
+        self.game = game 
    
 class Student(Family, School, Sports):
     def __init__(self, address, id, level, game) -> None:
         School.__init__(self, id, level)
         Sports.__init__(self, game)
         Family.__init__(self, address)    
+
