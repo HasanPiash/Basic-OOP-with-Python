ab2a1d8072ca65fa77722cb55d05d13e6974f1c9
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-09-03 22:28:01 +0600

    Update singleton

--- singleton.py
+++ singleton.py
@@ -1,12 +1,10 @@
 # singleton --> one single instance
 # if you want a new instance, you will get the old one (already created ) instance
-  
 class Singleton:   
     __instance = None
     def __init__(self) -> None:
         if Singleton.__instance is None:
-            Singleton.__instance = self
-        
+            Singleton.__instance = self     
         else:
             raise Exception('This is Singleton. Already have an instance, use that one by calling get_instance method')
        
@@ -19,9 +17,7 @@ class Singleton:
 first = Singleton.get_instance()
 second = Singleton.get_instance()
 third = Singleton.get_instance()
-
 print(first)
 print(second)
 print(third)
-
 # last = Singleton()
