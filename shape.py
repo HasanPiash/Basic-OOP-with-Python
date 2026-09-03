54dc68121bf54e51e4320a5700627044946a1246
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-09-03 22:26:29 +0600

    Update shape.py

--- shape.py
+++ shape.py
@@ -1,5 +1,5 @@
 from math import pi
-class Shape: 
+class Shape:
     def __init__(self, name) -> None:
         self.name = name
 
@@ -7,15 +7,13 @@ class Rectangle(Shape):
     def __init__(self, name, length, width) -> None:
         self.length = length
         self.width = width        
-        super().__init__(name)
-    
+        super().__init__(name)   
     def area(self):
         return self.length * self.width
         
 class Circle(Shape):   
     def __init__(self, name, radius) -> None:
         self.radius = radius
-        super().__init__(name)
-         
+        super().__init__(name)      
     def area(self):
         return pi * self.radius*self.radius
