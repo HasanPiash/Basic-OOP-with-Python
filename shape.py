a5e24e77c19079b750e2beb84259f6c9882d6e11
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-09-03 21:23:45 +0600

    Update shape.py

--- shape.py
+++ shape.py
@@ -1,15 +1,13 @@
 from math import pi
-class Shape:
+class Shape: 
     def __init__(self, name) -> None:
         self.name = name
-
 
 class Rectangle(Shape):
     def __init__(self, name, length, width) -> None:
         self.length = length
         self.width = width        
         super().__init__(name)
-
     
     def area(self):
         return self.length * self.width
