d8712b93dc5c7d2a7c96d0c03a3035061aa71e50
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-09-03 22:25:48 +0600

    Update polymorphism.py

--- polymorphism.py
+++ polymorphism.py
@@ -1,45 +1,37 @@
 # poly --> many (multiple)
-# morph --> shape 
-     
-class  Animal:
+# morph --> shape     
+class  Animal: 
     def __init__(self, name) -> None:
         self.name = name
         
     def make_sound(self):
         print('animal making some sound')
     
-class Cat(Animal):
+class Cat(Animal): 
     def __init__(self, name) -> None:
-        super().__init__(name)
-         
+        super().__init__(name)     
     def make_sound(self):
         print('meow meow')
      
 class Dog(Animal):
     def __init__(self, name) -> None:
         super().__init__(name)
-    
     def make_sound(self):
         print('gheu gheu')
    
 class Goat(Animal):
     def __init__(self, name) -> None:
-        super().__init__(name)
-       
+        super().__init__(name)   
     def make_sound(self):
         print('beh beh beh')
      
 don = Cat('Real Don')
 don.make_sound()
-    
 shepard = Dog('Local Shephard')
-shepard.make_sound()
-    
+shepard.make_sound()   
 mess = Goat('L M')
-mess.make_sound()
-    
+mess.make_sound()   
 less = Goat('gora gori')
-
 animals = [don, shepard, mess, less]
 for animal in animals:
     animal.make_sound()
