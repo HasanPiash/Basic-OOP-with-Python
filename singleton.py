fd9238266ec18ebfec0834677c50d90826775148
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-09-04 23:32:10 +0600

    Update singleton.py

--- singleton.py
+++ singleton.py
@@ -1,6 +1,6 @@
 # singleton --> one single instance
 # if you want a new instance, you will get the old one (already created ) instance
-class Singleton:   
+class Singleton:
     __instance = None
     def __init__(self) -> None:
         if Singleton.__instance is None:
