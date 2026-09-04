6b5f0d04e50678af89e9729b77d10c927dec7734
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-09-04 21:12:39 +0600

    Update static.py

--- static.py
+++ static.py
@@ -2,11 +2,10 @@
 # static method @staticmethod
 # class method @classmethod
 # differences between static method and class method
-    
 class Shopping:
     cart = [] #class attribute #static attribute
     origin = 'china'
-       
+
     def __init__(self, name, location) -> None:
         self.name = name  # instance attribute
         self.location = location   
@@ -19,16 +18,11 @@ class Shopping:
     @staticmethod
     def multiply(a, b):
         result = a*b
-        
         print(result)
-
     @classmethod
     def hudai_dekhi(self, item):
         print(self.name)
-        
         print('hudai dekhi kintu kinmu just ac er hawa khaite aschi', item)
-
-
 basundara = Shopping('basu en dhara', 'not popular location')
 # basundara.purchase('lungi', 500, 1000)
 basundara.hudai_dekhi('lungi')
@@ -37,6 +31,5 @@ Shopping.hudai_dekhi('Lungi')
 
 #static method
 Shopping.multiply(4,6) 
-
 
 # basundara.multiply(6,9)
